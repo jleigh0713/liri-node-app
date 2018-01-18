ed9960a64ca7de0f2fa38465232543844f6a69de
Jessica Leigh <jleigh@pietech.com>
2018-01-18 15:01:56 -0500

    inital stuf done

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.0XV3DByiscxaH7xl7xe2xe2Yk,
+  consumer_secret: process.env.Tg1cVw8w2rKKqZt4jKJmfgUwekPCSAROWByp1f6X0KdAlBzcYE,
+  access_token_key: process.env.954057982890954752-Jdcw5bbqR8d3IsYz3do62EIQgi2RXVB,
+  access_token_secret: process.env.R0JF6Ue2FwuoOV8zXrzzSUrr2A5zxVTxlP8DlBHSvhhMS
+};
+
+exports.spotify = {
+  id: process.env.64462369fa4d474c96da447a6daf588b,
+  secret: process.env.5a6f7ae0bedf414c8e28b62cf38fd952
+};
