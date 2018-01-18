184bf7500e21d9f6a09a855b369988738a1ce758
Jessica Leigh <jleigh@pietech.com>
2018-01-18 17:52:28 -0500

    final commit

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-console.log('this is loaded');
+console.log('keys.js loaded');
 
 exports.twitter = {
-  consumer_key: process.env.0XV3DByiscxaH7xl7xe2xe2Yk,
+  consumer_key: process.env.V3DByiscxaH7xl7xe2xe2Yk,
   consumer_secret: process.env.Tg1cVw8w2rKKqZt4jKJmfgUwekPCSAROWByp1f6X0KdAlBzcYE,
-  access_token_key: process.env.954057982890954752-Jdcw5bbqR8d3IsYz3do62EIQgi2RXVB,
+  access_token_key: process.env.Jdcw5bbqR8d3IsYz3do62EIQgi2RXVB,
   access_token_secret: process.env.R0JF6Ue2FwuoOV8zXrzzSUrr2A5zxVTxlP8DlBHSvhhMS
 };
 
-exports.spotify = {
+/*exports.spotify = {
   id: process.env.64462369fa4d474c96da447a6daf588b,
   secret: process.env.5a6f7ae0bedf414c8e28b62cf38fd952
-};
+};*/
--- liri.js
+++ liri.js
@@ -1,4 +1,86 @@
+
+var dotenv = require("dotenv").config();
+var command = process.argv[2];
+var movie = process.argv[3];
 var keys = require('./keys.js');
 var request = require('request');
 var twitter = require('twitter');
-var spotify = require('spotify');
+var fs = require('fs');
+var inquirer = require('inquirer');
+var client = new twitter(keys.twitter);
+//var spotify = require('spotify');
+
+
+//Defining function to get my tweets from twitter account
+function getTweets() 
+{
+	var myInfo = {screen_name: '4truthonly7Demo', count: 20};
+
+	client.get('statuses/user_timeline', myInfo, function(err, tweets, response) 
+	{
+		//grab an error if there is one and display it
+		if (err) 
+		{
+       		console.log('Error occurred: ' + err);
+        	return;
+        //if there is not an error
+    	} 
+    	else if (!err) 
+    	{
+    	// Log the tweets to the console along with when they were created
+	  		tweets.forEach(function(tweet) 
+	  		{
+	  				console.log('Tweet: ' + tweet.text + '\nCreated: ' + tweet.created_at);
+	  		});
+	  	}
+	});
+}
+
+
+function movieThis(movie)
+{
+  var URL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true';
+
+  request(URL, (error, response, body) =>
+  {
+    if(!error && response.statusCode == 200)
+    {
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMdB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+      console.log("the country it was produced in was: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+    } 
+    else
+    {
+      console.log('Error' + error);
+    }
+    if(movie === null)
+    {
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" + "\nit's on Netflix!");
+    }
+  });
+}
+
+switch (command)
+{
+	case 'my-tweets':
+	getTweets();
+	break;
+
+	case 'move-this':
+	movieThis(movie);
+	break;
+}
+
+
+
+movieThis();
+getTweets();
+
+
+//checking to see if the file is even working
+console.log("Hello");
